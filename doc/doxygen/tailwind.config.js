b6c2e0726e32d7d4bb2635579ea2992d78bc7a1c
AnnsAnn <git@annsann.eu>
2024-10-23 09:58:17 +0200

    docs: Use riot color schema

--- doc/doxygen/tailwind.config.js
+++ doc/doxygen/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./header.html", "./footer.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "riot-green": "#40A687",
+        "riot-red": "#BC202A",
+      }
+    },
   },
   plugins: [],
 }
