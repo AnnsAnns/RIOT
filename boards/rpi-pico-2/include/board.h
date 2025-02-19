57a25b56d1b7ff194f5057ec591b48802087a49c
AnnsAnn <git@annsann.eu>
2025-02-19 15:39:19 +0100

    pico2: simplify for now

--- boards/rpi-pico-2/include/board.h
+++ boards/rpi-pico-2/include/board.h
@@ -23,22 +23,10 @@
 #include "cpu.h"
 #include "periph_conf.h"
 #include "periph_cpu.h"
-#include "periph/gpio.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/**
- * @name    LED configuration
- * @{
- */
-#define LED0_PIN        GPIO_PIN(0, 25)
-#define LED0_ON         do {SIO->GPIO_OUT_SET = 1UL << 25;} while (0)
-#define LED0_OFF        do {SIO->GPIO_OUT_CLR = 1UL << 25;} while (0)
-#define LED0_TOGGLE     do {SIO->GPIO_OUT_XOR = 1UL << 25;} while (0)
-#define LED0_NAME       "LED(Green)"
-/** @} */
 
 #ifdef __cplusplus
 }
