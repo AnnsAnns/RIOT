d8fe1fa67741fc22f12a40667dfdd130cfa70e63
AnnsAnn <git@annsann.eu>
2025-03-19 16:11:05 +0100

    pico2: example with led flash

--- examples/basic/minimal/main.c
+++ examples/basic/minimal/main.c
@@ -1,3 +1,43 @@
+
+#include "RP2350.h"
+
+__STATIC_FORCEINLINE void configure_led()
+{
+  // function 5 = SIO 
+  IO_BANK0->GPIO25_CTRL = 0x05;
+  PADS_BANK0->GPIO25 = 0x34;
+  // enable output
+  SIO->GPIO_OE_SET = 0x01U << 25;
+}
+
+__STATIC_FORCEINLINE void turn_led_on()
+{
+  SIO->GPIO_OUT_SET = 0x01U << 25;
+}
+
+__STATIC_FORCEINLINE void turn_led_off()
+{
+  SIO->GPIO_OUT_CLR = 0x01U << 25;
+}
+
+__STATIC_FORCEINLINE void flip_led()
+{
+  SIO->GPIO_OUT_XOR = 0x01U << 25;
+}
+
+
 int main(void) {
+    // This will cause our program to never return
+    // but also means that we can be sure nothing else is running
+    while (1) {
+        for (int i = 0; i < 75000000; i++) __NOP();
+        flip_led();
+    }
+    
+    volatile int i = 1;
+    
+    while (i) {
+    }
+
     return 0;
 }
