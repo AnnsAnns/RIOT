23f4b4bd5a1c1ab8a000efb1015fe1f3ee5295cd
AnnsAnn <git@annsann.eu>
2025-03-20 11:25:44 +0100

    pico2: gpio via saul

--- boards/rpi-pico-2/include/board.h
+++ boards/rpi-pico-2/include/board.h
@@ -1,6 +1,7 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include "cpu.h"
 #include "periph_conf.h"
 #include "periph_cpu.h"
 #include "cpu_conf.h"
@@ -8,6 +9,17 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/**
+ * @name    LED configuration
+ * @{
+ */
+#define LED0_PIN        GPIO_PIN(0, 25)
+#define LED0_ON         do {SIO->GPIO_OUT_SET = 1UL << 25;} while (0)
+#define LED0_OFF        do {SIO->GPIO_OUT_CLR = 1UL << 25;} while (0)
+#define LED0_TOGGLE     do {SIO->GPIO_OUT_XOR = 1UL << 25;} while (0)
+#define LED0_NAME       "LED(Green)"
+/** @} */
 
 #ifdef __cplusplus
 }
