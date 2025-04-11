b6d9f130118e8369bc6ae540229a218c11c5353d
AnnsAnn <git@annsann.eu>
2025-04-11 15:29:07 +0200

    doc/guides: integrate external links to api doc/rust

--- doc/guide/astro.config.mjs
+++ doc/guide/astro.config.mjs
@@ -1,44 +1,74 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://riotguide.annsann.eu",
-    integrations: [
-      starlight({
-        title: "Riot Documentation",
-        social: [
-          { icon: 'github', label: 'GitHub', href: 'https://github.com/riot-os/riot' },
-          { icon: 'mastodon', label: 'Mastodon', href: 'https://fosstodon.org/@RIOT_OS' },
-          { icon: 'matrix', label: 'Matrix', href: 'https://matrix.to/#/#riot-os:matrix.org' },
-        ],
-        sidebar: [
-          {
-            label: "RIOT in a Nutshell",
-            items: [
-              { label: "Introduction", slug: "index" },
-              { label: "RIOT Vision", slug: "general/vision" },
-              "general/community_process",
-              "general/structure",
-            ],
-          },
-          {
-            label: "Build System",
-            autogenerate: { directory: "build-system" },
-          },
-        ],
-        customCss: [],
-        logo: {
-          src: "./src/assets/riot-logo.svg",
-          replacesTitle: true,
+  site: "https://riotguide.annsann.eu",
+  integrations: [
+    starlight({
+      title: "Riot Documentation",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/riot-os/riot",
         },
-        plugins: [],
-        editLink: {
-          baseUrl:
-            "https://github.com/AnnsAnns/RIOT/tree/newguides/doc/guides",
+        {
+          icon: "mastodon",
+          label: "Mastodon",
+          href: "https://fosstodon.org/@RIOT_OS",
         },
-      }),
-    ],
-  });
-  
+        {
+          icon: "matrix",
+          label: "Matrix",
+          href: "https://matrix.to/#/#riot-os:matrix.org",
+        },
+      ],
+      sidebar: [
+        {
+          label: "External Documentation",
+          items: [
+            {
+              label: "API Documentation",
+              link: "https://doc.riot-os.org",
+              badge: { text: "Doxygen", variant: "tip" },
+            },
+            {
+              label: "Rust Documentation",
+              link: "https://doc.riot-os.org/rustdoc/latest/riot_doc_helpers/",
+              badge: { text: "Rustdoc", variant: "tip" },
+            },
+          ],
+        },
+        {
+          label: "RIOT in a Nutshell",
+          items: [
+            { label: "Introduction", slug: "index" },
+            { label: "RIOT Vision", slug: "general/vision" },
+            "general/community_process",
+            "general/structure",
+          ],
+        },
+        {
+          label: "Build System",
+          items: [
+            "build-system/build_system",
+            "build-system/build_system_basics",
+            "build-system/build-in-docker",
+            "build-system/advanced_build_system_tricks",
+          ],
+        },
+      ],
+      customCss: [],
+      logo: {
+        src: "./src/assets/riot-logo.svg",
+        replacesTitle: true,
+      },
+      plugins: [],
+      editLink: {
+        baseUrl: "https://github.com/AnnsAnns/RIOT/tree/newguides/doc/guides",
+      },
+    }),
+  ],
+});
