ca84aa2e196bde4629e7639d41b7058751a438b8
AnnsAnn <git@annsann.eu>
2025-08-21 14:37:24 +0200

    Move cpu.h to riscv_common itself

---
+++ cpu/riscv_common/include/cpu.h
@@ -0,0 +1,28 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Tom Hert <git@annsann.eu>
+ * SPDX-FileCopyrightText: 2025 HAW Hamburg
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+#pragma once
+
+/**
+ * @ingroup     cpu_riscv_common
+ * @{
+ *
+ * @file
+ * @brief       CPU specific definitions
+ */
+
+#include "cpu_conf.h"
+#include "cpu_common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
