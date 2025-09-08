cb8c48af2affc0c535c33291ed3ad5cb163107d8
AnnsAnn <git@annsann.eu>
2025-09-08 13:06:14 +0200

    stash

--- cpu/rp2350_riscv/include/cpu.h
+++ cpu/rp2350_riscv/include/cpu.h
@@ -50,29 +50,10 @@ typedef uint32_t gpio_t;
 
 #include "irq_arch.h"
 #include "clic.h"
-#include "cpu_conf.h"
-#include "cpu_common.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/**
- * @brief   Returns the address of running application in flash
- */
-static inline uint32_t cpu_get_image_baseaddr(void)
-{
-    extern uint8_t _start;
-    return (uint32_t)&_start;
-}
-
-/**
- * @brief   Starts another image in flash
- */
-static inline void cpu_jump_to_image(uint32_t addr)
-{
-    __asm__ volatile ("jr %0" :: "r" (addr));
-}
 
 #ifdef __cplusplus
 }
