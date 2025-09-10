73c8be7f323ef156f1de83e0e67a02ced336028e
AnnsAnn <git@annsann.eu>
2025-09-10 13:24:17 +0200

    doc/guides: migrate creating modules

--- doc/starlight/astro.config.mjs
+++ doc/starlight/astro.config.mjs
@@ -87,6 +87,7 @@ export default defineConfig({
               label: "Advanced",
               items: [
                 "advanced_tutorials/creating_application",
+                "advanced_tutorials/creating_modules",
                 "advanced_tutorials/device_drivers",
                 "advanced_tutorials/porting_boards",
               ],
